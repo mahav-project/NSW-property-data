da93c428b44c1596bafa4b211945c1559f5628b5
maha-project <mahavir_parekh@yahoo.com>
2026-04-06 21:54:54 +1000

    CICD Update

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,13 @@
 terraform {
   required_version = ">= 1.6"
 
+  backend "s3" {
+    bucket  = "nsw-property-terraform-state"
+    key     = "terraform.tfstate"
+    region  = "ap-southeast-2"
+    encrypt = true
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
